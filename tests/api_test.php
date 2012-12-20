8a0436a38d579cfea191a08b5adf817f2fe6675f
Jason Peak <jpeak5@lsu.edu>
2012-12-19 20:22:01 -0600

    changes

--- tests/api_test.php
+++ tests/api_test.php
@@ -11,10 +11,15 @@ class api_testcase extends advanced_testcase{
     protected $user_idnumbers;
     protected function setUp(){
         setConfig();
+        
+        /**
+         * idnumbersw ending in 0,1 are Mike The Tiger
+         * ending in 9 is a user (Bill Cat) that does not exist in the t-card system
+         */
         $this->user_idnumbers = array(
-            890000000,
-            890000001,
-            890000009,
+//            890000000,
+//            890000001,
+//            890000009,
             890001202,
             890001366,
             890001632
@@ -33,13 +38,65 @@ class api_testcase extends advanced_testcase{
         $update     = get_config('block_my_picture', 'update_url');
         $webservice = get_config('block_my_picture', 'webservice_url');
         
-        $this->assertEquals($update, 'https://tt.lsu.edu/api/v2/jpg/kZabUZ6TZLcsYsCnV6KW/photos/lsuid/%s/update');
+        $this->assertEquals($update,     'https://tt.lsu.edu/api/v2/jpg/kZabUZ6TZLcsYsCnV6KW/photos/lsuid/%s/update');
         $this->assertEquals($webservice, 'https://tt.lsu.edu/api/v2/jpg/kZabUZ6TZLcsYsCnV6KW/photos/lsuid/%s?skip_place_holder=true');
-        $this->assertEquals($ready, 'https://tt.lsu.edu/api/v2/json/kZabUZ6TZLcsYsCnV6KW/photos/recently_updated/%s');
+        $this->assertEquals($ready,      'https://tt.lsu.edu/api/v2/json/kZabUZ6TZLcsYsCnV6KW/photos/recently_updated/%s');
         
+        //output
+        mtrace(sprintf("ready_url,      corresponds to webservice 'recently_updated' method/URL is set as   %s", $ready));
+        mtrace(sprintf("update_url,     corresponds to webservice 'update'           method/URL is set as   %s", $update));
+        mtrace(sprintf("webservice_url, corresponds to webservice 'show'             method/URL is set as   %s", $webservice));
         mtrace("\n--\n");
     }
     
+    /**
+     * This test method corresponds to the API for queueing a user photo as ready 
+     * for reprocessing.
+     * NB: <em>This method shedules a process to find any copy the user's photo 
+     * from another source. As a result, there is a delay between the time this 
+     * request is issued and the updated photo is available.</em>
+     * 
+     * @global type $CFG
+     */
+//    public function testTriggerUpdateForSampleUsers(){
+//        global $CFG;
+//        $this->resetAfterTest(true);
+//        $success = array();//we need to know what was scheduled successfully
+//        foreach($this->user_idnumbers as $idnumber){
+//
+//            mtrace(sprintf("Triggering update for user with ID %s from update URL\n", $idnumber));
+//
+//            $url = get_config('block_my_picture', 'update_url');
+//
+//            $hash = hash("sha256", $idnumber);
+//            $curl = new curl();
+//            $json = $curl->post(sprintf($url, $hash));
+//
+//            $obj = json_decode($json);
+//
+//            mtrace(sprintf("request url: %s\n", sprintf($url, $hash)));
+//
+//            mtrace("\nRAW JSON:\n");
+//            mtrace(print_r($json));
+//
+//    //        mtrace("\nJSON decoded:\n");
+//    //        mtrace(print_r($obj));
+//
+//            $this->assertTrue(is_object($obj), sprintf("expected an object, got something else...\n"));
+//            $this->assertNotEmpty($obj);
+//            
+//            if(object_property_exists($obj, 'success')){
+//                $this->assertEquals(true, $obj->success->status);
+//                $this->assertEquals('Photo update scheduled', $obj->success->message);
+//                $success[] = $obj;
+//            }else if($obj->error){
+//                $this->assertEquals('User not found', $obj->error);
+//            }
+//            mtrace(sprintf("Updates successfully triggered for %d users", count($success)));
+//            mtrace("\n--\n");
+//        }
+//        return $success;
+//    }
     
     public function testGetImageFromWebserviceUrl(){
         global $CFG;
@@ -61,52 +118,16 @@ class api_testcase extends advanced_testcase{
             mtrace(sprintf("%s - %s\n", $idnumber, $url));
 
             fclose($fp);
-            $this->assertGreaterThan(0, filesize($fullpath), sprintf("filesize is zero for path %s", $fullpath));
+            $this->assertGreaterThan(1, filesize($fullpath), sprintf("filesize is zero for path %s", $fullpath));
         }
         mtrace("\n--\n");
-        return true;
+        
         
     }
     
     
-    /**
-     * This test method corresponds to the API for queueing a user photo as ready 
-     * for reprocessing.
-     * NB: <em>This method shedules a process to find any copy the user's photo 
-     * from another source. As a result, there is a delay between the time this 
-     * request is issued and the updated photo is available.</em>
-     * 
-     * @global type $CFG
-     */
-    public function testGetJsonFromUpdateUrl(){
-        global $CFG;
-        $this->resetAfterTest(true);
 
-        $idnumber = 890003719;//genLsuId();
-        mtrace(sprintf("Getting single image for user with ID %s\n", $idnumber));
-        
-        $url = get_config('block_my_picture', 'update_url');
 
-        $hash = hash("sha256", $idnumber);
-        $curl = new curl();
-        $json = $curl->post(sprintf($url, $hash));
-
-        $obj = json_decode($json);
-
-        mtrace(sprintf("request url: %s\n", sprintf($url, $hash)));
-        
-//        mtrace("\nRAW JSON:\n");
-//        mtrace(print_r($json));
-        
-//        mtrace("\nJSON decoded:\n");
-//        mtrace(print_r($obj));
-        
-        $this->assertTrue(is_object($obj), sprintf("expected an object, got something else...\n"));
-        $this->assertNotEmpty($obj);
-        $this->assertEquals(1, $obj->success->status);
-        $this->assertEquals('Photo update scheduled', $obj->success->message);
-        mtrace("\n--\n");
-    }
     
     /**
      * This test mimics a check for recently updated photos.
@@ -117,27 +138,34 @@ class api_testcase extends advanced_testcase{
      * @global type $CFG
      * @return type
      */
-    public function testListPhotosRecentlyUpdatedNaturally(){
-
-        global $CFG;
-        $this->resetAfterTest(true);
-        
-        
-        $time = time() - 3600*24*7;
-        mtrace(sprintf("Getting a list of photos updated since %s...", strftime('%F %T', $time)));
-        
-        $result = mypic_get_users_updated_pictures($time);
-        $this->assertTrue(is_array($result));
-        $this->assertNotEmpty($result);
-        
-//        mtrace(print_r($result));
-        mtrace(sprintf("got %s users\n", count($result)));
-        foreach($result as $user){
-            echo $user->idnumber." ";
-        }
-        mtrace("\n--\n");
-        return($result);
-    }
+//    public function testListPhotosRecentlyUpdatedNaturally(){
+//
+//        global $CFG;
+//        $this->resetAfterTest(true);
+//        
+//        
+//        $time = time() - 3600*24*7;
+//        mtrace(sprintf("Getting a list of photos updated since %s...", strftime('%F %T', $time)));
+//        
+//        $result = mypic_get_users_updated_pictures($time);
+//        $this->assertTrue(is_array($result));
+//        $this->assertNotEmpty($result);
+//        
+////        mtrace(print_r($result));
+//        $limit = 10;
+//        mtrace(sprintf("got %s users, listing the first %d...\n", count($result), $limit));
+//        $i=0;
+//        
+//        foreach($result as $user){
+//            echo $user->idnumber." ";
+//            if($i>$limit){
+//                break;
+//            }
+//            $i++;
+//        }
+//        mtrace("\n--\n");
+//        return($result);
+//    }
     
 
     /**
@@ -145,75 +173,105 @@ class api_testcase extends advanced_testcase{
      * @global type $CFG
      * @return type
      */
-    public function testListPhotosRecentlyUpdatedByUs($updated){
+//    public function testListPhotosRecentlyUpdatedByUs($updated){
+//
+//        global $CFG;
+//        $this->resetAfterTest(true);
+//        
+//        if($updated){
+//
+//            $time = time() - 120;
+//            mtrace(sprintf("Getting a list of photos updated updated BY THIS TEST SUITE at %s; expect  %s...\n", strftime('%F %T', $time), count($this->user_idnumbers)));
+//            $result = mypic_get_users_updated_pictures($time);
+//            $this->assertTrue(is_array($result));
+//            $this->assertNotEmpty($result);
+//            
+//        
+//            $updated_ids = array();
+//            if($result){
+//                foreach($result as $user){
+//                    $updated_ids[] = $user->idnumber;
+//                }
+//            }
+//            foreach($this->user_idnumbers as $preset){
+//                if(in_array($preset, $updated_ids)){
+//                    unset($preset);
+//                }
+//            }
+//            
+//            foreach($result as $user){
+//             
+//                echo $user->idnumber." ";
+//                
+//            }
+//            $this->assertCount(0,$this->user_idnumbers);
+//    //        mtrace(print_r($result));
+//            mtrace("\n--\n");
+//            
+//        }
+//    }
+    
+    
+
+    
+    
+    
+    /**
+     * 
+     * @global type $CFG
+     * @return type
+     */
+    public function testListPhotosRecentlyUpdated(){
 
         global $CFG;
         $this->resetAfterTest(true);
+        $offset = 0;
         
-        if($updated){
+        mtrace("Getting lists of users updated in the last 12 hours");
+        
+     
 
-            $time = time() - 120;
-            mtrace(sprintf("Getting a list of photos updated updated BY THIS TEST SUITE at %s; expect  %s...\n", strftime('%F %T', $time), count($this->user_idnumbers)));
+            $offset < 12*3600;
+            $time = time() - $offset;
+
             $result = mypic_get_users_updated_pictures($time);
+            mtrace(sprintf("\nGetting a list of photos updated updated since %s...got %s\n", strftime('%F %T', $time), count($result)));
             $this->assertTrue(is_array($result));
-            $this->assertNotEmpty($result);
+//            $this->assertNotEmpty($result);
+            $res_ids = array();
+            foreach($result as $user){
+                echo $user->idnumber." ";
+                $res_ids[] = $user->idnumber;
+            }
+            mtrace("\nsample user ids are as follows");
+            foreach($this->user_idnumbers as $id){
+                echo $id." ";
+            }
+            
             
         
-            $updated_ids = array();
-            if($result){
-                foreach($result as $user){
-                    $updated_ids[] = $user->idnumber;
-                }
-            }
-            foreach($this->user_idnumbers as $preset){
-                if(in_array($preset, $updated_ids)){
-                    unset($preset);
-                }
-            }
-            
-            foreach($result as $user){
-             
-                echo $user->idnumber." ";
-                
-            }
-            $this->assertCount(0,$this->user_idnumbers);
-    //        mtrace(print_r($result));
-            mtrace("\n--\n");
-            
+//        foreach($result as $obj){
+//            $this->assertTrue(in_array($obj->idnumber,$this->user_idnumbers));
+//            
+//        }
+        
+        mtrace("\ndiff between sample id array and updated array is:");
+        $diff = array_diff($res_ids, $this->user_idnumbers);
+        foreach($diff as $d){
+            echo $d." ";
         }
+        
+        foreach($this->user_idnumbers as $sample){
+            mtrace(sprintf("asserting that sample id %s is in the list returned from the so-called ready_url",$sample));
+            $this->assertTrue(in_array($sample, $res_ids));
+        }
+        
+        
+       mtrace("\n--\n");
+        
     }
     
-    
-//    /**
-//     * @depends testListPhotosRecentlyUpdatedNaturally
-//     */
-//    public function testDownloadRecentlyUpdatedPhotos($result){
-//        global $CFG;
-//        $this->resetAfterTest();
-//        mtrace("Testing image download for each of those users retrieved as RECENTLY UPDATED...\n");
-//        foreach($result as $user_photo_record){
-//            $idnumber = $user_photo_record->idnumber;
-//
-//            $hash = hash("sha256", $idnumber);
-//
-//            $filename = $idnumber . '.jpg';
-//            $fullpath = $CFG->dataroot . '/temp/' . $filename;
-//            $fp = fopen($fullpath, 'w');
-//
-//            $curl = new curl();
-//
-//            $url = sprintf(get_config('block_my_picture', 'webservice_url'), $hash);
-//            $curl->download(array(array('url' => $url, 'file' => $fp)));
-//            fclose($fp);
-//            
-//            mtrace(sprintf("%s - %s\n", $idnumber, $url));
-//
-//            $this->assertGreaterThan(0, filesize($fullpath), sprintf("filesize is zero for path %s", $fullpath));
-//        }
-//        mtrace("\n--\n");
-//        
-//    }
-    
+
 }
 
 ?>
